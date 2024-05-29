26d0248c6e9c7ca8a5bc3b5d720d5ed037341f0a
air <air@airs-MacBook-Air.local>
2024-05-29 22:22:37 +0530

    drivng license identiier

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let age = prompt("Enter your age");
+
+if (a>=18) {
+    console.log("if k andr wala block")
+    alert("you are eligible  for driving") 
+}
+else{
+    console.log("else  k andr wala block")
+    alert("you are not aligeble for drivin")
+}
